e504db7d369bebb2cd9df44d5bbfa7cae351bb90
Kaama <kaama.io.21.relay@gmail.com>
2023-03-02 18:38:52 +0100

    Created angular-app

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kaama-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'kaama.live';
+}
